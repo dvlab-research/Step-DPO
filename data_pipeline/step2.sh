a513e810e296f63aeea323acd52e26c7e2de20df
Xin Lai <laixin112233@qq.com>
2024-07-07 12:05:22 +0800

    upload data pipeline

--- data_pipeline/step2.sh
+++ data_pipeline/step2.sh
@@ -1,5 +1,5 @@
-export OPENAI_BASE_URL="https://api.ai-gaochao.cn/v1/" # input openai base_url here
-export OPENAI_API_KEY="sk-rnwwDzET7PLzJz7X5372CfB450Ce45EfB073Fc5590D42cA6" # input openai api_key here
+export OPENAI_BASE_URL="" # input openai base_url here
+export OPENAI_API_KEY="" # input openai api_key here
 
 python3 data_pipeline/locate_error_by_gpt4.py \
     --prompt "qwen2-boxed-step" \
